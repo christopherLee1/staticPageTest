9c8a5998e694bbdb2061eaac2483237456a2a0b2
Christopher Lee <chmalee@ucsc.edu>
2017-02-14 10:48:31 -0800

    Initial commit. Stage template test script and links to check.

---
+++ staticPageTest.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re, sys
+
+class GenomeTest(unittest.TestCase):
+    def __init__(self, testname, host):
+        super(GenomeTest, self).__init__(testname)
+        self.base_url = host
+        self.driver = webdriver.Chrome("/Users/christopherLee/Downloads/chromedriver")
+        self.driver.implicitly_wait(30)
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.driver.switch_to_alert()
+        except NoAlertPresentException as e: return False
+        return True
+    
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
+    
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    suite = unittest.TestSuite()
+    suite.addTest(GenomeTest("test_genome_test", sys.argv[1]))
+    unittest.TextTestRunner().run(suite)
