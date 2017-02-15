d99452bbdd44da34799adc1aa5c54282578e9716
Christopher Lee <chmalee@ucsc.edu>
2017-02-15 08:25:35 -0800

    Final version of test script

--- staticPageTest.py
+++ staticPageTest.py
@@ -1,4 +1,10 @@
-# -*- coding: utf-8 -*-
+#!/bin/env/python3 
+'''
+This small script takes a file of urls as input and simply
+loads the web page and scrolls to the bottom, somewhat slow
+enough for the eye to catch any errrors.
+'''
+
 from selenium import webdriver
 import sys
 
@@ -9,22 +15,17 @@ def scroll_down(driver, increment):
       scheight += increment
 
 driver = webdriver.Chrome("/Users/christopherLee/Downloads/chromedriver")
-#driver.implicitly_wait(300)
 links = open(sys.argv[1], 'r')
 for link in links:
-   driver.get(link)
-   # after page loads send javascript to slowly scroll down pagea
-   page_height = driver.execute_script("return document.body.scrollHeight")
-   #scheight = 0
-   # three classes of pages, short, medium, long
-   if page_height < 2000:
-      #while scheight < 1:
-      #   driver.execute_script("window.scrollTo(0, document.body.scrollHeight*%s);" % scheight)
-      #   scheight += .001
-      scroll_down(driver, .001)
-   #elif page_height < 10000:
-   #   scroll_down(driver, .0005)
+   if link[0:1] == ('#'):
+      continue
    else:
-      scroll_down(driver, .0001)
+      driver.get(link)
+      # after page loads send javascript to slowly scroll down pagea
+      page_height = driver.execute_script("return document.body.scrollHeight")
+      if page_height < 2000: # smaller pages
+         scroll_down(driver, .001)
+      else: # larger pages need a slightly slower scroll
+         scroll_down(driver, .0001)
 
 driver.quit()  
